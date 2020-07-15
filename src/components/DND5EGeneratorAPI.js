54bf966ac83b6a59414ef787ee2dc611523ef35a
Erin Davis <51665717+erin-davis@users.noreply.github.com>
2020-07-15 00:40:33 -0500

    worked on the api and am trying to figure out async/await to see if that'll help

--- src/components/DND5EGeneratorAPI.js
+++ src/components/DND5EGeneratorAPI.js
@@ -4,16 +4,42 @@ import PotentialCharaSheet from "./PotentialCharaSheet.js";
 
 const DNDGenerator = props =>{
   const [dndClass, setDndClass] = useState({})
+  //const [dndValues, setDndValues] = useState([]);
+  const [dndRes, setDndRes] = useState({});
+  const [apiValues, setApiValues] = useState([]);
+
+  // const dndApiData = async () =>{
+  //   const dndRes = await axios.get(`https://www.dnd5eapi.co/api`);
+  //   setDndRes(dndRes.data);
+  //   let dndValues = Object.values(dndRes.data);
+  //   console.log(dndRes.data);
+  // };
+  //
+  // useEffect(()=>{
+  //   dndApiData();
+  // }, [])
+// ? `/api/classes` : `/api`
 
   useEffect(() =>{
     axios
     .get(`https://www.dnd5eapi.co/api`)
       .then(res =>{
+        setApiValues(Object.values(res.data));
         console.log("this is the response", res.data);
       })
       .catch(err =>{
         console.log("this is the error", err);
       })
+    // axios
+    // .get(`https://www.dnd5eapi.co${apiValues.map(() =>{
+    //   return `${apiValues}`
+    // })}`)
+    //     .then(res =>{
+    //       console.log('this is from the second api call', res);
+    //     })
+    //     .catch(err =>{
+    //       console.log('from the second api call, i messed up like this: ', err);
+    //     })
   }, []);
 
   return null;
