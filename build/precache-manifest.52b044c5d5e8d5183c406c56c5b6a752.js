509f52a7a91fee0fa74036836771c3694b330d38
Erin Davis <51665717+erin-davis@users.noreply.github.com>
2020-11-20 16:16:06 -0600

    removed /build from gitignore and added server.js file

---
+++ build/precache-manifest.52b044c5d5e8d5183c406c56c5b6a752.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd42c827b8c955104bfbc313ba6ffcfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1aebc402bd3844110345",
+    "url": "/static/css/main.7608d633.chunk.css"
+  },
+  {
+    "revision": "02dc3bb9ab37666e862a",
+    "url": "/static/js/2.c298d4ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c298d4ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aebc402bd3844110345",
+    "url": "/static/js/main.ca7e1f06.chunk.js"
+  },
+  {
+    "revision": "0d75b1899dba00489f37",
+    "url": "/static/js/runtime-main.88d10d25.js"
+  }
+]);
