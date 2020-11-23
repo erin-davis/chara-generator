8f69e3c332c14197fcbd13c369c7fec258f5acf3
Erin Davis <51665717+erin-davis@users.noreply.github.com>
2020-11-22 19:29:55 -0600

    Updates

---
+++ precache-manifest.961ca59683a4feabff9b88292ffd75b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f56dee56cd47eee4dd2edb453cc059",
+    "url": "/chara-generator/index.html"
+  },
+  {
+    "revision": "c920a46cb808a5c580fb",
+    "url": "/chara-generator/static/css/main.8a59511b.chunk.css"
+  },
+  {
+    "revision": "809dfa3476e28c684732",
+    "url": "/chara-generator/static/js/2.991269de.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chara-generator/static/js/2.991269de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c920a46cb808a5c580fb",
+    "url": "/chara-generator/static/js/main.c9c27d1a.chunk.js"
+  },
+  {
+    "revision": "11a82d3e7869c95c6fb4",
+    "url": "/chara-generator/static/js/runtime-main.36eda323.js"
+  }
+]);
