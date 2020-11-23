9a21cfc60ad4c9f6ae4bfc5dcc533aca61e11cc3
Erin Davis <51665717+erin-davis@users.noreply.github.com>
2020-11-23 00:01:48 -0600

    Updates

--- precache-manifest.961ca59683a4feabff9b88292ffd75b7.js
+++ precache-manifest.961ca59683a4feabff9b88292ffd75b7.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0e7c9ace57bb1e30a10bd12871a45fb4",
+    "revision": "52f56dee56cd47eee4dd2edb453cc059",
     "url": "/chara-generator/index.html"
   },
   {
-    "revision": "8674c8fd3b2a7c6656e4",
+    "revision": "c920a46cb808a5c580fb",
     "url": "/chara-generator/static/css/main.8a59511b.chunk.css"
   },
   {
-    "revision": "88015c1e66eded5404b9",
-    "url": "/chara-generator/static/js/2.d3505e52.chunk.js"
+    "revision": "809dfa3476e28c684732",
+    "url": "/chara-generator/static/js/2.991269de.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/chara-generator/static/js/2.d3505e52.chunk.js.LICENSE.txt"
+    "url": "/chara-generator/static/js/2.991269de.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8674c8fd3b2a7c6656e4",
-    "url": "/chara-generator/static/js/main.f06c8d9e.chunk.js"
+    "revision": "c920a46cb808a5c580fb",
+    "url": "/chara-generator/static/js/main.c9c27d1a.chunk.js"
   },
   {
     "revision": "11a82d3e7869c95c6fb4",
